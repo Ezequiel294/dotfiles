291055e741ea96c9b0e601642d170fd35a8a6a48
Ezequiel Buck <ezequiel.bm04@gmail.com>
2023-09-26 13:33:59 -0600

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Ensure the system is up-to-date
+sudo pacman -Syu --noconfirm
+
+# Install yay
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+# Install packages from .pkg file
+cat .pkg | xargs yay -S --needed --noconfirm
+
+# Change shell to fish
+chsh
+# Fish route /usr/bin/fish
+
+reboot
