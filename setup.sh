93ad63cdf63221233ebfe0ef9f7b7fd0d7361e9e
Ezequiel Buck <119618678+Ezequiel294@users.noreply.github.com>
2023-09-27 15:27:07 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,13 +4,23 @@
 sudo pacman -Syu --noconfirm
 
 # Install yay
-git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+if ! pacman -Qi yay &> /dev/null; then
+    git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+fi
 
 # Install packages from .pkg file
 cat .pkg | xargs yay -S --needed --noconfirm
 
 # Change shell to fish
 # Fish route /usr/bin/fish
-chsh
+if [[ "$SHELL" != *fish* ]]; then
+    chsh
+fi
 
-reboot
+# Ask to reboot the system
+read -p "Do you want to reboot the PC? (y/n): " choice
+case "$choice" in
+  y|Y ) echo "Rebooting..."; reboot;;
+  n|N ) echo "You can reboot manually later.";;
+  * ) echo "Invalid input. Please enter y or n.";;
+esac
