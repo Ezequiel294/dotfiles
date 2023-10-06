3e50e82e9ee133ea94100586b98d0493bf0dd75e
Ezequiel Buck <ezequiel.bm04@gmail.com>
2023-10-06 01:57:47 -0600

    nvim alt -> mod

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -32,36 +32,35 @@ from libqtile.widget import Spacer
 import arcobattery
 
 mod = "mod4"
-alt = "mod1"
 terminal = guess_terminal()
 
 keys = [
     # A list of available commands that can be bound to keys can be found
     # at https://docs.qtile.org/en/latest/manual/config/lazy.html
     # Switch between windows
-    Key([alt], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([alt], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([alt], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([alt], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([alt], "space", lazy.layout.next(),
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(),
         desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([alt, "shift"], "h", lazy.layout.shuffle_left(),
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
         desc="Move window to the left"),
-    Key([alt, "shift"], "l", lazy.layout.shuffle_right(),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
         desc="Move window to the right"),
-    Key([alt, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([alt, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([alt, "control"], "h", lazy.layout.grow_left(),
+    Key([mod, "control"], "h", lazy.layout.grow_left(),
         desc="Grow window to the left"),
-    Key([alt, "control"], "l", lazy.layout.grow_right(),
+    Key([mod, "control"], "l", lazy.layout.grow_right(),
         desc="Grow window to the right"),
-    Key([alt, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([alt, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([alt], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
@@ -203,11 +202,11 @@ screens = [
                 foreground=colors[2],
                 background=colors[1],
             ),
-            #widget.CheckUpdates(
+            # widget.CheckUpdates(
             #    distro="Arch",
             #    display_format="󰮯 {updates}",
             #    colour_have_updates=colors[6],
-            #),
+            # ),
             widget.Systray(
                 background=colors[1],
                 icon_size=20,
