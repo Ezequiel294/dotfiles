0a160782a8dc3e64d8a5ba18595304cb3bb35c51
Ezequiel Buck <119618678+Ezequiel294@users.noreply.github.com>
2023-10-08 10:26:06 -0600

    Update lsp.lua

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,16 +1,24 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp_zero.default_keymaps({buffer = bufnr})
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
--- see :help lsp-zero-guide:integrate-with-mason-nvim
--- to learn how to use mason.nvim with lsp-zero
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    handlers = {
-        lsp_zero.default_setup,
-    }
+  ensure_installed = {
+            'pylsp',
+            'clangd',
+            'bashls',
+            'lua_ls',
+            'tsserver',
+            'jsonls',
+            'cssls',
+            'html',
+  },
+  handlers = {
+    lsp_zero.default_setup,
+  },
 })
