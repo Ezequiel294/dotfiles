09de12ede8b91cb1e73b1f1da8bfd17d8c971cd2
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-05-12 22:03:29 -0600

    nvim removed treesitter autotag functionality

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,16 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function()
-        require('nvim-treesitter.configs').setup {
-            sync_install = false,
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            autotag = {
-                enable = true,
-            },
-        }
-    end
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        config = function()
+            require('nvim-treesitter.configs').setup {
+                sync_install = false,
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+            }
+        end
+    },
 }
