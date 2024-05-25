40f29aa045bcaea52a78ba179eb0b92294834b07
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-05-24 19:06:31 -0600

    bat catppuccin theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ set fish_greeting ""
 # Aliases
 
 alias grep "grep --color=auto"
-alias cat "bat --style=plain --paging=never"
+alias cat "bat"
 alias ls "exa --group-directories-first"
 alias tree "exa -T"
 alias tmux "tmux -2"
