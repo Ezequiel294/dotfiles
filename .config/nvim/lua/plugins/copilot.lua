dca166a74479f50991df9a87a29107c952ee2e6e
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-06-05 23:07:10 -0600

    nvim add copilot chat plugin

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,32 @@
 return {
-	"github/copilot.vim",
+	{ "github/copilot.vim" },
+
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "github/copilot.vim" },
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		opts = {
+			debug = true, -- Enable debugging
+			-- See Configuration section for rest
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+		config = function()
+			require("CopilotChat").setup({
+				window = {
+					layout = "float",
+                    width = 0.7,
+                    height = 0.7,
+				},
+			})
+
+			vim.keymap.set("n", "<leader>ct", ":CopilotChatToggle<CR>")
+			vim.keymap.set("v", "<leader>ce", ":CopilotChatExplain<CR>")
+			vim.keymap.set("v", "<leader>cr", ":CopilotChatReview<CR>")
+			vim.keymap.set("v", "<leader>cf", ":CopilotChatFix<CR>")
+			vim.keymap.set("v", "<leader>co", ":CopilotChatOptimize<CR>")
+		end,
+	},
 }
