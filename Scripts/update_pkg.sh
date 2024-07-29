e3c43a2f122835c7f41b8cc15a9c5a5636599b79
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-07-28 20:27:38 -0600

    excluded packages from the list

--- Scripts/update_pkg.sh
+++ Scripts/update_pkg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm $HOME/Scripts/Arch/pkg.txt
-pacman -Qqe | tr '\n' ' ' | sed 's/.$//' > $HOME/Scripts/Arch/pkg.txt
+pacman -Qqe | grep -v -e 'intel-ucode' -e 'amd-ucode' -e 'paru' | tr '\n' ' ' | sed 's/.$//' > $HOME/Scripts/Arch/pkg.txt
