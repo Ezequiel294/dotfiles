91adc12e7b1990a9ecbe3f28eac49ec8ef266460
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-08-14 14:03:44 -0600

    imporved cleaning script

--- Scripts/clean.sh
+++ Scripts/clean.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
-# Run paccache -d to show available removal space
-paccache -d
+# Clean journalctl logs
+echo -e "\nCleaning journalctl logs..."
+sudo journalctl -p 3 -xb --vacuum-time=7d
 
-# Ask the user if they want to continue with the cleaning
-read -p "Do you want to continue with cleaning? (y/n): " choice
-if [[ $choice == "y" ]]; then
-  echo "Cleaning..."
-  paccache -r
-  echo "Cleaning completed."
-else
-  echo "Cleaning aborted."
+# Run paccache -d to show available removal space
+echo -e "\nRunning paccache -d to show available removal space..."
+paccache -d
+echo "Cleaning..."
+paccache -r
+
+# Clean ununsed dependencies
+echo -e "\nCleaning ununsed dependencies..."
+paru --clean
+echo -e "\n"
+
+# Calculate the size of the .cache directory
+cache_size=$(du -sh ~/.cache 2>/dev/null | awk '{print $1}')
+echo "The .cache directory is currently using $cache_size of space."
+# Warn about files larger than 100MB
+large_files_found=$(find ~/.cache -type f -size +100M 2>/dev/null)
+if [[ -n $large_files_found ]]; then
+    echo "Warning: There are files larger than 100MB in your .cache directory."
+    echo "Largest files in the .cache directory:"
+    find ~/.cache -type f -exec du -h {} + 2>/dev/null | sort -hr | head -n 5
 fi
+# Prompt user for cleaning the .cache directory
+read -p "Do you want to clean the .cache directory? [Y/n]: " response
+if [[ -z "${response}" || "${response}" =~ ^[Yy]$ ]]; then
+    rm -rf ~/.cache/*
+    echo ".cache directory cleaned."
+else
+    echo "cleaning .cache directory aborted."
+fi
+
+echo -e "\nclean.sh script finished.\n"
