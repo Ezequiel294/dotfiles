5c1f61c18846c76712e0040655caaf9163348da2
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-08-17 20:30:40 -0600

    added git config to post-install script

--- Scripts/post-install.sh
+++ Scripts/post-install.sh
@@ -19,6 +19,7 @@ cat $HOME/Scripts/pkg.txt | xargs paru -S --needed --noconfirm
 # Set environment variables
 echo -e "\nSetting environment variables..."
 echo "QT_QPA_PLATFORMTHEME=qt6ct" | sudo tee -a /etc/environment
+echo 'GTK_THEME="Breeze-Dark"' | sudo tee -a /etc/environment
 echo "EDITOR=nvim" | sudo tee -a /etc/environment
 echo "VISUAL=nvim" | sudo tee -a /etc/environment
 echo "TERM=kitty" | sudo tee -a /etc/environment
@@ -28,6 +29,24 @@ echo "Environment variables have been set"
 echo -e "\nAdding user to the libvirt group..."
 sudo usermod -aG libvirt $USER
 echo "User has been added to the libvirt group"
+
+# Setting up Git
+echo -e "\nSetting up Git..."
+read -p "Enter your Git name: " git_name
+git config --global user.name "$git_name"
+read -p "Enter your Git email: " git_email
+git config --global user.email "$git_email"
+# Install Git Credential Manager
+echo -e "\nInstalling Git Credential Manager..."
+cd Repos
+curl -L https://aka.ms/gcm/linux-install-source.sh | sh
+git-credential-manager configure
+cd
+echo "Setting Git built-in cache..."
+git config --global credential.credentialStore cache
+git config --global credential.cacheOptions "--timeout 1800"
+echo "Git Credential Manager has been installed"
+echo "Git has been set up"
 
 # Install yarn and neovim npm package
 echo -e "\nInstalling yarn and npm neovim packages..."
