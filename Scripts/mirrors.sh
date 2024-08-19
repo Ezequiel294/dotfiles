7a36957ab34ee647f222e1afe034a632a1fdfba1
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-08-19 08:33:00 -0600

    in mirror.sh 10 -> 25 mirrors

--- Scripts/mirrors.sh
+++ Scripts/mirrors.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo reflector --verbose --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --verbose --latest 25 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 sudo pacman -Syy
