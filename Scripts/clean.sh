f3caf66e67710e1fd99e1cdec2b509ebc7ec41ad
Ezequiel Buck <119618678+Ezequiel294@users.noreply.github.com>
2024-12-13 20:36:17 -0600

    no longer using paru

--- Scripts/clean.sh
+++ Scripts/clean.sh
@@ -8,14 +8,9 @@ sudo journalctl -p 3 -xb --vacuum-time=7d
 echo -e "\nRunning paccache"
 paccache -r
 
-# Clean ununsed dependencies
-echo -e "\nCleaning ununsed dependencies..."
-paru --clean
-echo -e "\n"
-
 # Calculate the size of the .cache directory
 cache_size=$(du -sh ~/.cache 2>/dev/null | awk '{print $1}')
-echo "The .cache directory is currently using $cache_size of space."
+echo -e "\nThe .cache directory is currently using $cache_size of space."
 # Warn about files larger than 100MB
 large_files_found=$(find ~/.cache -type f -size +100M 2>/dev/null)
 if [[ -n $large_files_found ]]; then
