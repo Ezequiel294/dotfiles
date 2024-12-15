70bfda7becff2ba5487d166d6c7311ece11a103c
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-12-14 22:02:55 -0600

    ignore yay package

--- Scripts/update_pkg.sh
+++ Scripts/update_pkg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm $HOME/Scripts/pkg.txt
-pacman -Qqe | grep -v -e 'intel-ucode' -e 'amd-ucode' -e 'paru' -e 'qemu-guest-agent' -e 'virtualbox-guest-utils' -e 'nvidia' -e 'radeon' -e 'vulkan' | tr '\n' ' ' | sed 's/.$//' >$HOME/Scripts/pkg.txt
+pacman -Qqe | grep -v -e 'intel-ucode' -e 'amd-ucode' -e 'qemu-guest-agent' -e 'virtualbox-guest-utils' -e 'nvidia' -e 'radeon' -e 'vulkan' -e 'yay' | tr '\n' ' ' | sed 's/.$//' >$HOME/Scripts/pkg.txt
