5a422ea0a6bfd68f85cbfd8c9c0bbbe0e9a93bb7
Ezequiel Buck <ezequiel.bm04@gmail.com>
2025-07-16 18:49:23 -0600

    finishing branch to wsl

--- Scripts/post-install.sh
+++ Scripts/post-install.sh
@@ -9,38 +9,10 @@ echo -e "\n"
 read -p "Enter your Git name: " git_name
 read -p "Enter your Git email: " git_email
 
-# Set environment variables
-echo -e "\nSetting environment variables..."
-echo "QT_QPA_PLATFORMTHEME=qt6ct" | sudo tee -a /etc/environment
-echo 'GTK_THEME="Breeze-Dark"' | sudo tee -a /etc/environment
-echo "BROWSER=firefox-developer-edition" | sudo tee -a /etc/environment
-echo "EDITOR=nvim" | sudo tee -a /etc/environment
-echo "VISUAL=nvim" | sudo tee -a /etc/environment
-echo "TERM=ghostty" | sudo tee -a /etc/environment
-echo -e "Environment variables have been set\n"
-
-# Install AUR helper yay
-echo -e "\nInstalling yay..."
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
-cd
-rm -rf yay-bin
-echo -e "yay has been installed\n"
-
 # Install packages
 echo -e "\nInstalling packages..."
-yay $(cat $HOME/Scripts/pkg.txt) -S --needed --noconfirm
+sudo pacman $(cat $HOME/Scripts/pkg.txt) -S --needed --noconfirm
 echo -e "Packages installed correctly\n"
-
-# QEMU setup
-echo -e "\nSetting up QEMU..."
-echo "Enabling libvirtd service..."
-sudo systemctl enable libvirtd
-echo "Adding user to the libvirt group..."
-sudo usermod -aG libvirt $USER
-echo -e "User has been added to the libvirt group\n"
 
 # Setting up Git
 echo -e "\nSetting up Git..."
@@ -48,11 +20,7 @@ git config --global user.name "$git_name"
 git config --global user.email "$git_email"
 # Install Git Credential Manager
 echo -e "\nInstalling Git Credential Manager..."
-curl -L https://aka.ms/gcm/linux-install-source.sh | sh
-git-credential-manager configure
-echo "Setting Git built-in cache..."
-git config --global credential.credentialStore cache
-git config --global credential.cacheOptions "--timeout 1800"
+git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
 echo "Git Credential Manager has been installed"
 echo -e "Git has been set up\n"
 
@@ -76,11 +44,6 @@ echo -e "\nInstalling bat catppuccin theme..."
 wget -P "$(bat --config-dir)/themes" https://github.com/catppuccin/bat/raw/main/themes/Catppuccin%20Mocha.tmTheme
 bat cache --build
 echo -e "Bat catppuccin theme has been installed\n"
-
-# Enable login manager service
-echo -e "\nEnabling login manager service..."
-sudo systemctl enable ly
-echo -e "Services have been enabled\n"
 
 fastfetch
 
